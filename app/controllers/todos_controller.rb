6a48bea93ec1dc2f2740372906ddf37c109854ad
Megan Smith <meg.eli.smi@gmail.com>
2017-01-26 13:45:38 -0800

    all basic CRUD functions working

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,12 +18,14 @@ class TodosController < ApplicationController
 
 	def update 
 		@todo = Todo.find(params[:id])
-		@todo.update
+		@todo.update(todo_params)
+		redirect_to todos_path
 	end
 
 	def destroy
   		@todo = Todo.find(params[:id])
   		@todo.destroy
+  		redirect_to todos_path
 	end
 
 	private 
