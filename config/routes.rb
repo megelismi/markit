f99a0474b3fdeff36639e469f8879acf6810429c
Megan Smith <meg.eli.smi@gmail.com>
2017-01-26 18:02:57 -0800

    home and about pages added, some styling added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 	resources :todos
-	root 'todos#index'
+	root 'static_pages#home'
+	get  '/about',   to: 'static_pages#about'
+	get '/todos', 	  to: 'todos#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
